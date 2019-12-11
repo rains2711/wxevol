bb48a873b70743fa9dfedf5fa7354a3ea709cc2f
lh245368455 <245368455@qq.com>
2019-12-11 17:43:19 +0800

    lh

--- dao/src/main/java/com/cn/wanxi/dao/user/UserDao.java
+++ dao/src/main/java/com/cn/wanxi/dao/user/UserDao.java
@@ -1,4 +1,29 @@
 package com.cn.wanxi.dao.user;
 
-public class UserDao {
+import com.cn.wanxi.common.IDao;
+
+import java.util.Map;
+
+public class UserDao implements IDao {
+
+
+    public Map<String, Object> findPage(Map<String, Object> map, Integer page, Integer size) {
+        return null;
+    }
+
+    public Map<String, Object> add(Map<String, Object> map) {
+        return null;
+    }
+
+    public Map<String, Object> update(Map<String, Object> map) {
+        return null;
+    }
+
+    public Map<String, Object> delete(Map<String, Object> map) {
+        return null;
+    }
+
+    public Map<String, Object> findById(Map<String, Object> map) {
+        return null;
+    }
 }
