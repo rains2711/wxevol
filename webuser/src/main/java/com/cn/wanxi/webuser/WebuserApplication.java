9f682eb7646f12db69021bb6a6bd5a558d7892aa
老李 <42735449+lirui167734886@users.noreply.github.com>
2019-12-13 11:20:28 +0800

    12.13

--- webuser/src/main/java/com/cn/wanxi/webuser/WebuserApplication.java
+++ webuser/src/main/java/com/cn/wanxi/webuser/WebuserApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+/**
+ * @author LiRui
+ */
 @Controller
 @SpringBootApplication
 public class WebuserApplication {
