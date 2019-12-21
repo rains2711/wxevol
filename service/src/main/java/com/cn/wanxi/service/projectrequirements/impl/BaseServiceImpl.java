f2ebd0862cc4d144d81febc3c6a3bbceb1446b03
罗跃权 <1260612638@qq.com>
2019-12-21 13:33:47 +0800

    webprojectrequirement BaseServiceImpl add

---
+++ service/src/main/java/com/cn/wanxi/service/projectrequirements/impl/BaseServiceImpl.java
@@ -0,0 +1,55 @@
+package com.cn.wanxi.service.projectrequirements.impl;
+
+import com.cn.wanxi.dao.projectrequirements.MyBatisBaseDao;
+import com.cn.wanxi.service.projectrequirements.IService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author luoyuequan
+ * @time 2019/12/21 13:11
+ */
+public abstract class BaseServiceImpl<Dao extends MyBatisBaseDao, Entity> implements IService<Entity> {
+
+    @Autowired
+    Dao baseDao;
+
+    /**
+     * 添加
+     *
+     * @param entity 实体
+     * @return 影响行数
+     */
+    public int insert(Entity entity) {
+        return baseDao.insertSelective(entity);
+    }
+
+    /**
+     * 删除
+     *
+     * @param entity 实体
+     * @return 影响行数
+     */
+    public int delete(Entity entity) {
+        return baseDao.deleteByPrimaryKey(entity);
+    }
+
+    /**
+     * 根据主键 查询
+     *
+     * @param entity 实体
+     * @return 查询实体
+     */
+    public Entity findById(Entity entity) {
+        return (Entity) baseDao.selectByPrimaryKey(entity);
+    }
+
+    /**
+     * 修改
+     *
+     * @param entity 实体
+     * @return 影响行数
+     */
+    public int update(Entity entity) {
+        return baseDao.updateByPrimaryKeySelective(entity);
+    }
+}
